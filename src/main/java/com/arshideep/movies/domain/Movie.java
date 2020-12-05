7fd7d1e6a51215cf9fab53eb75a364696b8bbac6
Richa Luthra <richaluthra.ca@gmail.com>
2020-12-05 15:37:45 -0500

    added movie class

--- src/main/java/com/arshideep/movies/domain/Movie.java
+++ src/main/java/com/arshideep/movies/domain/Movie.java
@@ -1,4 +1,19 @@
 package com.arshideep.movies.domain;
 
+import lombok.NonNull;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NonNull
+    private String movieName;
+    @NonNull private String movieLanguage;
+    private String genre;
+    @NonNull private Integer duration;
+
 }
